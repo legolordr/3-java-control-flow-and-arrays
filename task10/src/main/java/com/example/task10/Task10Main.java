f70e33b5c81b8e04819c3c67a5dd21ea16e3f859
legolordr <Ishmametev_den@mail.ru>
2026-09-07 09:19:38 +0500

    Ишмаметьев Денис. Задание 10

--- task10/src/main/java/com/example/task10/Task10Main.java
+++ task10/src/main/java/com/example/task10/Task10Main.java
@@ -1,17 +1,25 @@
 package com.example.task10;
 
+import java.lang.reflect.Array;
+
 public class Task10Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
         int[] arr = {7, 5, 9};
         System.out.println(numMin(arr));
-         */
     }
 
     static int numMin(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        int min = Integer.MAX_VALUE;
+        for (int n : arr){
+            if (n < min) min = n;
+        }
+
+        for (int i = arr.length - 1; i > 0; i--){
+            int value = Array.getInt(arr,i);
+            if (value == min) return i;
+        }
         return 0;
     }
 
